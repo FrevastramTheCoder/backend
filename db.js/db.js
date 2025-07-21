f97eb2d774b3f6fba1212dd591b9861d9f06ef46
Fredinand Pius Makiya <fredinand842@gmail.com>
2025-07-21 12:12:45 +0300

    update

--- db.js/db.js
+++ db.js/db.js
@@ -19,6 +19,30 @@
 
 // module.exports = pool;
 
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASS,
+//   port: Number(process.env.DB_PORT),
+//   ssl: { rejectUnauthorized: false },
+//   connectionTimeoutMillis: 10000,
+//   idleTimeoutMillis: 30000,
+//   max: 20,
+//   allowExitOnIdle: true
+// });
+
+// pool.on('error', (err, client) => {
+//   console.error('PostgreSQL Pool Error:', err.stack);
+// });
+
+// pool.on('connect', () => {
+//   console.log('PostgreSQL Pool Connected');
+// });
+
+// module.exports = pool;
 const { Pool } = require('pg');
 
 const pool = new Pool({
